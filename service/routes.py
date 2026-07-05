43f6eef7563d15bc0c5fa19cd9d3f291d481a8ab
OmmMalhotra02 <ommmalhotra02@gmail.com>
2026-07-05 15:59:12 -0400

    Identation fixes

--- service/routes.py
+++ service/routes.py
@@ -40,7 +40,10 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0], counter=count[1]) for count in COUNTER.items()]
+    counters = [
+        dict(name=count[0], counter=count[1])
+        for count in COUNTER.items()
+    ]
 
     return jsonify(counters)
 
@@ -56,7 +59,7 @@ def create_counters(name):
     if name in COUNTER:
         return abort
         (
-            status.HTTP_409_CONFLICT, 
+            status.HTTP_409_CONFLICT,
             f"Counter {name} already exists"
         )
 
@@ -78,8 +81,11 @@ def read_counters(name):
     app.logger.info("Request to Read counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, 
-        f"Counter {name} does not exist")
+        return abort
+        (
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
 
     counter = COUNTER[name]
     return jsonify(name=name, counter=counter)
@@ -93,8 +99,10 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, 
-        f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
 
     COUNTER[name] += 1
 
